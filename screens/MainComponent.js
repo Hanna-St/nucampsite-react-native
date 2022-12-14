e72b0d9cf9d20e5dc75a2bb800bfe5330357097c
Hanna Stsiapanava <stepanova.anna0103@gmail.com>
2022-12-14 14:49:43 -0600

    Modal

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -185,7 +185,10 @@ const Main = () => {
             <Drawer.Navigator
                 initialRouteName='Home'
                 drawerContent={CustomDrawerContent}
-                drawerStyle={{ backgroundColor: '#CEC8FF' }}
+                screenOptions={{
+                    headerShown: false,
+                    drawerStyle: { backgroundColor: '#CEC8FF' }
+                }}
             >
                 <Drawer.Screen 
                     name='Home'
